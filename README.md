32b6bac8bf768b8dbbe9af02c4c44d0a34590ad8
Anshul Shirude <34725764+AnshulShirude@users.noreply.github.com>
2023-01-22 11:08:47 -0500

    project

---
+++ README.md
@@ -0,0 +1,79 @@
+High Level Overview: In terms of a high level overview, there is a main message board and then there is a way to add a message.
+When adding a message, it takes in a few different things and then adds the message to the main message board. The message board is structured as a form style where you can check the message off to try and remove the message, but I did not get to that functionality yet.
+
+Components: The major component into play is the literall message that is added. Then within that message there is a title fiels, description, and there is a date. Those are all of the components that a message contains and are embedded inside of the message. There are also buttons on the pages but that is not completely a component.
+
+I fulfill the requirements for the project because users are able to type a message and add it to the message board. Then, users are able to see the message at the top of the screen. However, one of the things that this does not support is the ability to view each other's messages. For that you would need to have a database and update the database with the message. I hope to add that functionality in the future.
+
+Start: In order to start the application, cd into the folder and then assuming that you have all of the react dependencies, run ```npm start```. You may not need the react dependencies either, try running npm start while in the folder.
+
+# Getting Started with Create React App
+
+This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+
+## Available Scripts
+
+In the project directory, you can run:
+
+### `npm start`
+
+Runs the app in the development mode.\
+Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+
+The page will reload if you make edits.\
+You will also see any lint errors in the console.
+
+### `npm test`
+
+Launches the test runner in the interactive watch mode.\
+See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
+
+### `npm run build`
+
+Builds the app for production to the `build` folder.\
+It correctly bundles React in production mode and optimizes the build for the best performance.
+
+The build is minified and the filenames include the hashes.\
+Your app is ready to be deployed!
+
+See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
+
+### `npm run eject`
+
+**Note: this is a one-way operation. Once you `eject`, you can’t go back!**
+
+If you aren’t satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
+
+Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you’re on your own.
+
+You don’t have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn’t feel obligated to use this feature. However we understand that this tool wouldn’t be useful if you couldn’t customize it when you are ready for it.
+
+## Learn More
+
+You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
+
+To learn React, check out the [React documentation](https://reactjs.org/).
+
+### Code Splitting
+
+This section has moved here: [https://facebook.github.io/create-react-app/docs/code-splitting](https://facebook.github.io/create-react-app/docs/code-splitting)
+
+### Analyzing the Bundle Size
+
+This section has moved here: [https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size](https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size)
+
+### Making a Progressive Web App
+
+This section has moved here: [https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app](https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app)
+
+### Advanced Configuration
+
+This section has moved here: [https://facebook.github.io/create-react-app/docs/advanced-configuration](https://facebook.github.io/create-react-app/docs/advanced-configuration)
+
+### Deployment
+
+This section has moved here: [https://facebook.github.io/create-react-app/docs/deployment](https://facebook.github.io/create-react-app/docs/deployment)
+
+### `npm run build` fails to minify
+
+This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
---
+++ src/App.js
@@ -0,0 +1,137 @@
+import './App.css';
+import buttonpng from "./button1.png"
+import checkpng from "./check2.png"
+import cancelpng from "./cancel-button.png"
+import addTaskpng from "./addTask.png"
+import React, { useState } from 'react';
+
+function App() {
+  const [titleText, setTitleText] = useState("")
+  const [descriptionText, setDescriptionText] = useState("")
+  const [assigneeText, setAssigneeText] = useState("")
+  const [dueDateText, setDueDateText] = useState("")
+  const [tasks, setTasks] = useState([])
+  const [renderAdd, setRenderAdd] = useState(false)
+  // make a new const for a setSelectedTask useState
+  // when the checkmark is clicked then map through the list of tasks and the set the completed component of the task to true
+  const [selectedTask, setSelectedTask] = useState([])
+  // another section to tasks.map for the completed tasks
+  // const [task, setCancel] = useState([])
+  const handleChange = event => {
+    setTitleText(event.value);
+    setDescriptionText(event.value);
+    setAssigneeText(event.value);
+    setDueDateText(event.value);
+  };
+
+  function handleTaskAdded() {
+    setTasks([...tasks, { titleText, descriptionText, assigneeText, dueDateText, checked: false }]); // tasks is the current state and then you are updating with titleText
+    alert("Task added: " + titleText + ", Description: " + descriptionText + ", Assignee: " + assigneeText + ", Due Date: " + dueDateText);
+  }
+  // console.log(tasks) // testing component added to check if the tasks are being rendered properly
+
+  // this is the function used for the adding task button
+  function handleRenderShow() {
+    setRenderAdd(renderAdd => true)
+  }
+
+  // this is the function used for the canceling task button inside of the add-task page
+  function handleRenderCancel() {
+    setRenderAdd(renderAdd => false)
+  }
+
+  // When the checkbox is clicked, then you want to call a function stating that the task has been marked as completed
+  function handleTaskCompleted() {
+    var taskTest = document.getElementById("tasked")
+
+    if (taskTest.checked == true) {
+      alert("Task marked complete: " + titleText + ", Description: " + descriptionText + ", Assignee: " + assigneeText + ", Due Date: " + dueDateText);
+    }
+  }
+
+  // When the checkmark is clicked then the tasks which are marked as complete will be moved to the completed tab
+  function handleMoveCompleted() {
+    alert("The checkmarked tasks have been moved to the completed side");
+    return (
+      <div> {tasks.filter(tasks => tasks.checked).map(filteredTask => (
+        <li>
+          {filteredTask}
+        </li>
+      ))} </div>
+    );
+  }
+
+  return (
+    <React.Fragment>
+      <div className="App">
+        <div className="tasks-container">
+          <div className="tasks-title" > Message Board </div>
+          {
+            // filter out all of the things which are not completed and then render them in the home page
+            // need another function that wll do the opposite for the completed tasks page
+            //tasks.filter.map (item ...)
+            tasks.map(item => {
+              return <div>
+                <input class="individual-task-checkbox" type="checkbox" id="tasked" name="done" onClick={() => {
+                  setTasks(tasks.map(t => {
+                    if ( t.titleText == item.titleText )
+                    {
+                      alert(t.titleText + " marked complete!")
+                      return { ...t, checked: true }
+                    }
+                    return t
+                  }))
+                }} />
+                <button class="individual-task-button" onClick={(c) => setSelectedTask(c.target.value)}> {item.titleText} </button>
+              </div>
+            })
+          }
+
+          <img className="add-task-button" src={buttonpng} onClick={handleRenderShow} />
+          <img className="add-check-button" src={checkpng} />
+        </div>
+
+        {renderAdd && <div> <div className="add-task-container" >
+          <div className="add-task-title" > Add Message</div>
+          <div className="title" > Title: </div>
+          <div>
+            <form method="post" action-xhr="/submit-form" target="_top" class="main-form">
+              <div class="input">
+                <input class="input-title-box" type="text" name="name" id="title-box-form" value={titleText} onChange={(e) => setTitleText(e.target.value)} required />
+                <label for="form-name"></label>
+              </div>
+            </form>
+          </div>
+          <div className="description"> Description:</div>
+          <div>
+            <form method="post" action-xhr="/submit-form" target="_top" class="main-form">
+              <div class="input">
+                <input class="input-description-box" type="text" name="name" id="description-box-form" value={descriptionText} onChange={(e) => setDescriptionText(e.target.value)} required />
+                <label for="form-name"></label>
+              </div>
+            </form>
+          </div>
+          <div className="due-date">Date:</div>
+          <div>
+            <form method="post" action-xhr="/submit-form" target="_top" class="main-form">
+              <div class="input">
+                <input class="input-due-date-box" type="text" name="name" id="due-date-box-form" value={dueDateText} onChange={(e) => setDueDateText(e.target.value)} required />
+                <label for="form-name"></label>
+              </div>
+            </form>
+          </div>
+        </div>
+
+          <div className="add-task-container">
+            <img className="cancel-button" src={cancelpng} onClick={handleRenderCancel} />
+            <img className="mark-completed-task-button" src={addTaskpng} onClick={handleTaskAdded} />
+          </div>
+        </div>
+        }
+      </div>
+    </React.Fragment>
+
+  );
+}
+
+export default App;
